3d06d3c3d72756b4f5ae034e6f9645da039328d4
jocelyn <jboitelle@botdesign.net>
2020-03-19 19:01:53 +0100

    slider & control

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,31 +1,59 @@
+/*************** INIT ***************/
+
 const path = "public/img/";
 const images = ["alaska.jpg", "bears.jpg", "husky.jpg", "lac.jpg", "wolf.jpg"];
 const playingButton = document.getElementById('playing');
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
+const length = images.length;
+const timer = 1000;
 
-let index = 0;
+let index = 1;
 let playing = true;
-let slideInterval = setInterval(nextSlide, 1000);
+let slideInterval = setInterval(nextSlide, timer);
+
+document.slide.src = path + images[0];
+
+/*************** FUNCTION ***************/
 
 function nextSlide() {
     document.slide.src = path + images[index];
-    index = (index + 1) % images.length;
+    index = (index + 1) % length;
 }
 
-function pauseSlideshow(){
-	playingButton.innerHTML = 'Play';
+function pauseSlides() {
+    let icon = document.getElementsByClassName('fa-pause');
+    icon[0].className = " fas fa-play fa-3x ";
+    index = index - 1;
 	playing = false;
 	clearInterval(slideInterval);
 }
 
-function playSlideshow(){
-	playingButton.innerHTML = 'Pause';
+function playSlides(){
+    let icon = document.getElementsByClassName('fa-play');
+    icon[0].className = " fas fa-pause fa-3x ";
+    index = index + 1;
 	playing = true;
-	slideInterval = setInterval(nextSlide, 1000);
+	slideInterval = setInterval(nextSlide, timer);
 }
+
+/*************** ONCLICK ***************/
 
 playingButton.onclick = function() {
     if (playing)
-        pauseSlideshow();
+        pauseSlides();
     else
-        playSlideshow();
+        playSlides();
+};
+
+nextButton.onclick = function () {
+    index = (index + 1) % length;
+    document.slide.src = path + images[index];
+};
+
+prevButton.onclick = function () {
+    index = index - 1;
+    if (index < 0)
+        index = length - 1;
+    document.slide.src = path + images[index];
 };
